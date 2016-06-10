c9b2fb10a70ab97d9f07c855924dcd085d052e0a
Kyle Dugger <dugger.jk@gmail.com>
2016-06-10 10:48:09 -0500

    Initial project created with models set up, ready for first migration

---
+++ Music-History-TAEN/src/Music-History-TAEN/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music_History_TAEN.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string AlbumTitle { get; set; }
+        public string Artist { get; set; }
+        public int YearReleased { get; set; }
+
+        public ICollection<Track> TrackList { get; set; }
+    }
+}
---
+++ Music-History-TAEN/src/Music-History-TAEN/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music_History_TAEN.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+
+        public ICollection<Album> AlbumList { get; set; }
+    }
+}
