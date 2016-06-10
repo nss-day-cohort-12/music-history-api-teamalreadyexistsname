5a56003f714655e20db16d954ac5bcca5493785c
Kyle Dugger <dugger.jk@gmail.com>
2016-06-10 12:46:02 -0500

    Initial Migration added

---
+++ Music-History-TAEN/src/Music-History-TAEN/Models/MusicHistoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music_History_TAEN.Models
+{
+    public class MusicHistoryContext : DbContext
+    {
+        public MusicHistoryContext(DbContextOptions<MusicHistoryContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Album> Album { get; set; }
+        public DbSet<Track> Track { get; set; }
+        public DbSet<Artist> Artist { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
